6eaf6f222dc8adc157982ca5035fa99d43ae177f
Kath Pobee-Norris <kathwath@hotmail.com>
2017-01-12 10:12:13 +0000

    Docking station releases working bike; unit tests written and passing

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -3,5 +3,10 @@ require 'bike'
 describe Bike do
 
   it {is_expected.to respond_to :working?}
-  
+
+  it 'is expected that the bike is working' do
+    bike = Bike.new
+    expect(bike).to be_working
+  end
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,4 +4,14 @@ describe DockingStation do
 
 it {is_expected.to respond_to :release_bike}
 
+it 'docking station should release a new bike' do
+  station = DockingStation.new
+  expect(station.release_bike).not_to eq nil
+end
+
+it 'is expected that the bike should be working' do
+  bike = subject.release_bike
+  expect(bike).to be_working
+end
+
 end
