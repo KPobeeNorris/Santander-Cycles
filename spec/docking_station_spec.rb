a1b038ebb25e0c0903f4f83f9a158bdea5a33285
Kath Pobee-Norris <kathwath@hotmail.com>
2017-01-17 13:51:21 +0000

    Implemented new user story so that docking station now remembers the bike, and won't release a bike unless one is docked.  Included new tests, refactored old tests; all passing

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -9,12 +9,14 @@ it {is_expected.to respond_to(:dock).with(1).argument}
 it {is_expected.to respond_to :bike}
 
 it 'docking station should release a new bike' do
-  station = DockingStation.new
-  expect(station.release_bike).not_to eq nil
+  bike = Bike.new
+  subject.dock(bike)
+  expect(subject.release_bike).not_to eq nil
 end
 
 it 'is expected that the bike should be working' do
-  bike = subject.release_bike
+  bike = Bike.new
+  subject.dock(bike)
   expect(bike).to be_working
 end
 
@@ -23,5 +25,19 @@ it 'docks a bike' do
   subject.dock(bike)
   expect(subject.bike).to eq bike
 end
+
+  describe '#release_bike' do
+
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+
+    it 'doesn\'t release bikes if there aren\'t any at the docking station' do
+      expect{subject.release_bike}.to raise_error "No bikes available"
+    end
+
+  end
 
 end
