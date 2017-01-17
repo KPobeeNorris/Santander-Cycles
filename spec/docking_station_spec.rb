b0ce43d8e439d0a3fcd1d43d9b7dd81b4759d380
Kath Pobee-Norris <kathwath@hotmail.com>
2017-01-17 13:10:22 +0000

    Added dock method, attr_reader for bike and includes passing tests

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,6 +4,10 @@ describe DockingStation do
 
 it {is_expected.to respond_to :release_bike}
 
+it {is_expected.to respond_to(:dock).with(1).argument}
+
+it {is_expected.to respond_to :bike}
+
 it 'docking station should release a new bike' do
   station = DockingStation.new
   expect(station.release_bike).not_to eq nil
@@ -12,6 +16,12 @@ end
 it 'is expected that the bike should be working' do
   bike = subject.release_bike
   expect(bike).to be_working
+end
+
+it 'docks a bike' do
+  bike = Bike.new
+  subject.dock(bike)
+  expect(subject.bike).to eq bike
 end
 
 end
